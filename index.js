8ad697cb79b17a1f1a2c892022af76454c6e9480
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-01-17 07:57:36 +0900

    Test: 테스트 이미지 업로드

--- index.js
+++ index.js
@@ -39,6 +39,30 @@ apiRouter.post('/clue1', (req, res) => {
   res.status(200).send(responseBody);
 });
 
+apiRouter.post('/tree', (req, res) => {
+  const { action } = req.body;
+  const name = action.params.name;
+  const responseBody = {
+    version: '2.0',
+    templates: {
+      outputs: [
+        {
+          simpleImage: {
+            imageUrl:
+              'https://github.com/navynj/earlyblue/blob/main/asset/2024_%EA%B2%A8%EC%9A%B8%EC%BA%A0%ED%94%84%ED%8F%AC%EC%8A%A4%ED%84%B0_%EB%AA%A8%EB%8B%A5%EB%B6%88.png?raw=true',
+            altText: '팀원을 찾을 수 있는 나무조각',
+          },
+        },
+      ],
+    },
+    data: {
+      clue: `${clues[name]} : 테스트`,
+    },
+  };
+
+  res.status(200).send(responseBody);
+});
+
 app.listen(8000, () => {
   console.log('Server is running 🚀');
 });
